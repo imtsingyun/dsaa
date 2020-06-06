4b40baa33284343350f62c39d7fd02abf1374c9b
whaleu <56377217+whaleu@users.noreply.github.com>
2020-06-06 16:30:02 +0800

    learn fibonacci by golang

---
+++ ingolang/lession001/FibonacciTest.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+var n int
+
+func init() {
+	flag.IntVar(&n, "n", 0, "init value")
+}
+
+func main() {
+	flag.Parse()
+	startTime := time.Now().Unix()
+	result := fib1(n)
+	end := time.Now().Unix()
+	fmt.Printf("time %d, %v\n", (end - startTime), result)
+}
+
+func fib1(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fib1(n-1) + fib1(n-2)
+}
+
+func fib2(n int) int {
+	if n <= 1 {
+		return n
+	}
+	first := 0
+	second := 1
+	for i := 0; i < n-1; i++ {
+		sum := first + second
+		first = second
+		second = sum
+	}
+	return second
+}
--- injava/src/main/java/io/whaley/lession001/FibonacciTest.java
+++ injava/src/main/java/io/whaley/lession001/FibonacciTest.java
@@ -6,6 +6,7 @@ package io.whaley.lession001;
 public class FibonacciTest {
     
     /**
+     *
      * 0 1 1 2 3 5 8 13 21 34 55 89
      * @param args
      */
