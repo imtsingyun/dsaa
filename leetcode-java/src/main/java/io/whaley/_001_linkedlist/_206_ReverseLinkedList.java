91c142d5a323ed07613359de5b12e860aef69e2e
whaleu <suryaiwang@gmail.com>
2020-06-07 13:40:15 +0800

    206. Reverse Linked List

---
+++ leetcode-java/src/main/java/io/whaley/_001_linkedlist/_206_ReverseLinkedList.java
@@ -0,0 +1,31 @@
+package io.whaley._001_linkedlist;
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ * 206. Reverse Linked List
+ * 反转链表
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * 思路一：定义一个新的头节点，遍历链表，每次将原素插入新的链表的第一个位置
+ */
+public class _206_ReverseLinkedList {
+
+    public LinkedList.ListNode reverseList(LinkedList.ListNode head) {
+        // 定义一个新的头节点
+        LinkedList.ListNode newHead = null;
+        // 当前遍历到的节点
+        LinkedList.ListNode curr = head;
+        while (curr != null) {
+            // 指向当前节点的下一个节点
+            LinkedList.ListNode temp = curr.next;
+            // 将当前节点指向新链表的第一个节点
+            curr.next = newHead;
+            // 将新链表的头指针指向第一个节点
+            newHead = curr;
+            // 将原来链表的头节点指向
+            curr = temp;
+        }
+
+        return newHead;
+    }
+}
