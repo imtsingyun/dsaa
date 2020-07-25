cd047dd05dec9d3ef9a3ee4fe54b3da178e2d11e
Whaley <suryaiwang@gmail.com>
2020-07-25 21:52:44 +0800

    堆

---
+++ injava/src/main/java/io/whaley/algorithms/lession01_Sort/A03_HeapSort.java
@@ -0,0 +1,12 @@
+package io.whaley.algorithms.lession01_Sort;
+
+/**
+ * 堆排序
+ */
+public class A03_HeapSort {
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ injava/src/main/java/io/whaley/data_structure/lession012_heap/Heap.java
@@ -0,0 +1,11 @@
+package io.whaley.data_structure.lession012_heap;
+
+public interface Heap<E> {
+    int size();
+    boolean isEmpty();
+    void clear();
+    void add(E element);
+    E get();
+    E remove();
+    E replace(E element);
+}
